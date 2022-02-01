d33f8ca53100a40b9d04469e8e68e184508e2e6b
Yuri Maciel <54074819+yrimaciel@users.noreply.github.com>
2022-01-31 22:43:07 -0300

    code and test scoop options images from axios server

---
+++ src/pages/entry/Options.jsx
@@ -0,0 +1,31 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import ScoopOption from "./ScoopOption";
+import Row from "react-bootstrap/Row";
+
+export default function Options({ optionType }) {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:3030/${optionType}`)
+      .then((res) => {
+        setItems(res.data);
+      })
+      .catch((e) => {
+        console.log(e.response);
+      });
+  }, [optionType]);
+
+  const ItemComponent = optionType === "scoops" ? ScoopOption : null;
+  const optionItems = items.map((item) => {
+    return (
+      <ItemComponent
+        key={item.name}
+        name={item.name}
+        imagePath={item.imagePath}
+      />
+    );
+  });
+  return <Row>{optionItems}</Row>;
+}
