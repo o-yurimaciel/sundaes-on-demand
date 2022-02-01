3ff616bbe74ae8b5961bf34198e660fca234feff
Yuri Maciel <54074819+yrimaciel@users.noreply.github.com>
2022-01-31 21:40:31 -0300

    mocks folder

---
+++ src/mocks/handlers.js
@@ -0,0 +1,12 @@
+import { rest } from "msw";
+
+export const handlers = [
+  res.get("http://localhost:3030/scoops", (req, res, ctx) => {
+    return res(
+      ctx.json([
+        { name: "Chocolate", imagePath: "/images/chocolate.jpg" },
+        { name: "Vanilla", imagePath: "/images/vanilla.jpg" },
+      ])
+    );
+  }),
+];
--- src/pages/summary/test/SummaryForm.test.jsx
+++ src/pages/summary/test/SummaryForm.test.jsx
@@ -1,4 +1,9 @@
-import { render, screen, fireEvent } from "@testing-library/react";
+import {
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import SummaryForm from "../SummaryForm";
 
 test("Initial conditions", () => {
@@ -21,8 +26,30 @@ test("Checkbox enables button on first click and disables on second click", () =
   const confirmButton = screen.getByRole("button", {
     name: /confirm order/i,
   });
-  fireEvent.click(checkbox);
+  userEvent.click(checkbox);
   expect(confirmButton).toBeEnabled();
-  fireEvent.click(checkbox);
+  userEvent.click(checkbox);
   expect(confirmButton).toBeDisabled();
+});
+
+test("popover responds to hover", async () => {
+  render(<SummaryForm />);
+  const nullPopover = screen.queryByText(
+    /no ice cream will actually be delivered/i
+  );
+  expect(nullPopover).not.toBeInTheDocument();
+
+  const termsAndConditions = screen.getByText(/terms and conditions/i);
+
+  userEvent.hover(termsAndConditions);
+
+  const popover = screen.getByText(/no ice cream will actually be delivered/i);
+
+  expect(popover).toBeInTheDocument();
+
+  userEvent.unhover(termsAndConditions);
+
+  await waitForElementToBeRemoved(() =>
+    screen.queryByText(/no ice cream will actually be delivered/i)
+  );
 });
