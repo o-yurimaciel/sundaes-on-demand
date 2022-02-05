ceddf7ce0fc996cf57931171fb94c196a92f4f2d
Yuri Maciel <54074819+yrimaciel@users.noreply.github.com>
2022-02-05 13:53:57 -0300

    test with context

--- src/pages/entry/Options.jsx
+++ src/pages/entry/Options.jsx
@@ -4,10 +4,17 @@ import ScoopOption from "./ScoopOption";
 import ToppingOption from "./ToppingOption";
 import Row from "react-bootstrap/Row";
 import AlertBanner from "../common/AlertBanner";
+import { pricePerItem } from "../../constants";
+import { userOrderDetails } from "../../contexts/OrderDetails";
 
 export default function Options({ optionType }) {
   const [items, setItems] = useState([]);
   const [error, setError] = useState(false);
+  const [orderDetails, updateItemCount] = userOrderDetails();
+
+  function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
 
   useEffect(() => {
     axios
@@ -26,14 +33,29 @@ export default function Options({ optionType }) {
   }
 
   const ItemComponent = optionType === "scoops" ? ScoopOption : ToppingOption;
+  const title = capitalizeFirstLetter(optionType);
+
   const optionItems = items.map((item) => {
     return (
       <ItemComponent
         key={item.name}
         name={item.name}
         imagePath={item.imagePath}
+        updateItemCount={(itemName, newItemCount) =>
+          updateItemCount(itemName, newItemCount, optionType)
+        }
       />
     );
   });
-  return <Row>{optionItems}</Row>;
+
+  return (
+    <>
+      <h2>{title}</h2>
+      <p>{pricePerItem[optionType]} each</p>
+      <p>
+        {title} total: {orderDetails.totals[optionType]}
+      </p>
+      <Row>{optionItems}</Row>
+    </>
+  );
 }
